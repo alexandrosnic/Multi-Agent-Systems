9ea78b4309ac5320b3c59a93795e21b02b78db47
alexandrosnic <alexnic31@gmail.com>
2020-12-12 18:42:42 +0100

    added storage and shops

--- javaagents/src/main/java/massim/javaagents/agents/AlexAgent.java
+++ javaagents/src/main/java/massim/javaagents/agents/AlexAgent.java
@@ -44,6 +44,18 @@ public class AlexAgent extends Agent{
 
     private Random rand = new Random(17);
 
+	//***************      What I added:	***************
+	String carriedItems = "";
+	int numItems = 0;
+	private Map<String, Percept> shopsPercept = new HashMap<>();
+	private Map<String, Percept> dumpsPercept = new HashMap<>();
+	private String myJob;
+	private Set<String> jobsTaken = new HashSet<>();
+	private Queue<Action> actionQueue = new LinkedList<>();
+	private Loc dumpLoc;
+	// ****************************************************
+
+
     /**
      * Constructor
      *
@@ -60,7 +72,11 @@ public class AlexAgent extends Agent{
     @Override
     public Action step() {
 
-        // parse percepts
+		
+		Map<String, List<Percept>> shopsByItem = new HashMap<>();
+		Map<String, Percept> currentJobs = new HashMap<>();
+
+        // ********************		PERCEPTIONS		******************** //
         List<Percept> percepts = getPercepts();
         for (Percept percept : percepts) {
             switch(percept.getName()) {
@@ -97,8 +113,37 @@ public class AlexAgent extends Agent{
                     break;
                 case "massium": massium = getIntParam(percept, 0); break;
                 case "score": score = getIntParam(percept, 0); break;
+
+				case "dump": dumpsPercept.put(getStringParam(percept, 0), percept); break;
                 case "chargingStation": chargingStations.put(getStringParam(percept, 0), percept); break;
                 case "step": step = getIntParam(percept, 0); break;
+				case "item":
+                    carriedItems += " " + percept.toProlog();
+					numItems += 1;
+                    break;
+			}
+			if(actionQueue.size() == 0){
+                // parse info needed for planning
+                switch(percept.getName()){
+					case "job":
+						// remember all active jobs
+						currentJobs.putIfAbsent(stringParam(percept.getParameters(), 0), percept);
+						break;
+					case "shop":
+						// remember shops by what they offer
+						ParameterList stockedItems = listParam(percept, 4);
+						for(Parameter stock: stockedItems){
+							if(stock instanceof Function){
+								String itemName = stringParam(((Function) stock).getParameters(), 0);
+								int amount = intParam(((Function) stock).getParameters(), 2);
+								if(amount > 0){
+									shopsByItem.putIfAbsent(itemName, new ArrayList<>());
+									shopsByItem.get(itemName).add(percept);
+								}
+							}
+						}
+						break;
+				}
             }
         }
 
@@ -112,14 +157,13 @@ public class AlexAgent extends Agent{
 
         if(leader.equals(getName())) say("Score: " + score + " Massium: " + massium);
 
-        return act();
-    }
+		if(carriedItems.isEmpty()==false){
+            say("I carry " + carriedItems);
+        }
 
-    /**
-     * choose an action
-     * @return an action
-     */
-    private Action act() {
+
+
+		// ********************		ACTIONS		******************** //
 
         if(getName().equals(leader) && currentJob.equals("")) {
             for(Percept job: jobs.values()) {
@@ -205,7 +249,112 @@ public class AlexAgent extends Agent{
             return new Action("goto", new Numeral(exploreTarget.lat), new Numeral(exploreTarget.lon));
         }
 
-        return new Action("continue");
+
+		/*
+		if(atLoc(shopLoc)){
+		    String shopString = "";
+            double minDist = Double.MAX_VALUE;
+			if(numItems < 10) {
+				for(Percept p: shopsPercept.values()) {
+					double shopLat = getDoubleParam(p, 1);
+					double shopLon = getDoubleParam(p, 2);
+					double dist = Math.sqrt(Math.pow(lat - shopLat, 2) + Math.pow(lon - shopLon, 2));
+					if(dist < minDist) {
+						minDist = dist;
+						shopString = getStringParam(p, 0);
+					}
+				}
+			    Percept p = shopsPercept.get(shopString);
+                shopLoc = new Loc(getDoubleParam(p, 1), getDoubleParam(p, 2));
+				return new Action("goto", new Numeral(shopLoc.lat), new Numeral(shopLoc.lon));
+			}
+		}
+
+		if(atLoc(shopLoc)){
+
+		}
+		*/
+
+		// follow the plan if there is one
+        if(actionQueue.size() > 0) return actionQueue.poll();
+
+		// Associate a job to the agent
+		if (myJob == null){
+            Set<String> availableJobs = new HashSet<>(currentJobs.keySet());
+            availableJobs.removeAll(jobsTaken);
+            if(availableJobs.size() > 0){
+                myJob = availableJobs.iterator().next();
+                say("I will complete " + myJob);
+                jobsTaken.add(myJob);
+                broadcast(new Percept("taken", new Identifier(myJob)), getName());
+            }
+        }
+		// If already associated a job, carry it out
+		if(myJob != null){
+            // plan the job
+            // 1. acquire items
+            Percept job = currentJobs.get(myJob);
+            if(job == null){
+                say("I lost my job :(");
+                myJob = null;
+                return new Action("skip");
+            }
+            String storage = stringParam(job.getParameters(), 1);
+            ParameterList requirements = listParam(job, 4);
+            for (Parameter requirement : requirements) {
+                if(requirement instanceof Function){
+                    // 1.1 get enough items of that type
+                    String itemName = stringParam(((Function) requirement).getParameters(), 0);
+                    int amount = intParam(((Function) requirement).getParameters(), 1);
+                    if(itemName.equals("") || amount == -1){
+                        say("Something is wrong with this item: " + itemName + " " + amount);
+                        continue;
+                    }
+                    // find a shop selling the item
+                    List<Percept> shops = shopsByItem.get(itemName);
+                    if(shops.size() == 0){
+                        say("I cannot buy the item " + itemName + "; this plan won't work very well.");
+                    }
+                    else{
+                        say("I will go to the shop first.");
+                        // go to the shop
+                        Percept shop = shops.get(0);
+                        actionQueue.add(new Action("goto", new Identifier(stringParam(shop.getParameters(), 0))));
+                        // buy the items
+                        actionQueue.add(new Action("buy", new Identifier(itemName), new Numeral(amount)));
+                    }
+                }
+            }
+            // 2. get items to storage
+            actionQueue.add(new Action("goto", new Identifier(storage)));
+			// 2.1 store items
+			actionQueue.add(new Action("store"));
+            // 2.2 deliver items
+            actionQueue.add(new Action("deliver_job", new Identifier(myJob)));
+        }
+
+		if(numItems > 10) {
+			String dumpString = "";
+			double minDist = Double.MAX_VALUE;
+			for(Percept p: dumpsPercept.values()) {
+				double dumpLat = getDoubleParam(p, 1);
+				double dumpLon = getDoubleParam(p, 2);
+				double dist = Math.sqrt(Math.pow(lat - dumpLat, 2) + Math.pow(lon - dumpLon, 2));
+				if(dist < minDist) {
+					minDist = dist;
+					dumpString = getStringParam(p, 0);
+				}
+			}
+			Percept p = dumpsPercept.get(dumpString);
+            dumpLoc = new Loc(getDoubleParam(p, 1), getDoubleParam(p, 2));
+			return new Action("goto", new Numeral(dumpLoc.lat), new Numeral(dumpLoc.lon));
+		}
+
+		if(atLoc(dumpLoc)){
+			return new Action("dump");
+		}
+
+		return actionQueue.peek() != null? actionQueue.poll() : new Action("skip");
     }
 
     private boolean atLoc(Loc loc) {
@@ -243,6 +392,9 @@ public class AlexAgent extends Agent{
                 resourceNodes.put(name, message);
                 availableResources.add(resource);
                 break;
+			case "taken":
+                jobsTaken.add(stringParam(message.getParameters(), 0));
+                break;
             default:
                 say("I cannot handle a message of type " + message.getName());
         }
@@ -259,5 +411,27 @@ public class AlexAgent extends Agent{
 
     enum State {
         EXPLORE, RECHARGE, JOB, BUILD
+    }
+
+    private static ParameterList listParam(Percept p, int index){
+        List<Parameter> params = p.getParameters();
+        if(params.size() < index + 1) return new ParameterList();
+        Parameter param = params.get(index);
+        if(param instanceof ParameterList) return (ParameterList) param;
+        return new ParameterList();
+    }
+
+    public static String stringParam(List<Parameter> params, int index){
+        if(params.size() < index + 1) return "";
+        Parameter param = params.get(index);
+        if(param instanceof Identifier) return ((Identifier) param).getValue();
+        return "";
+    }
+
+    private static int intParam(List<Parameter> params, int index){
+        if(params.size() < index + 1) return -1;
+        Parameter param = params.get(index);
+        if(param instanceof Numeral) return ((Numeral) param).getValue().intValue();
+        return -1;
     }
 }
